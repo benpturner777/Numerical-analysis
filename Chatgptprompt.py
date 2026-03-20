b8a7a11281957043b1e9ba03c9395bd07397cd01
Ben Turner <benpturner2004@gmail.com>
2026-03-20 10:13:35 +0000

    Update Chatgptprompt.py

--- Chatgptprompt.py
+++ Chatgptprompt.py
@@ -2,5 +2,6 @@
 #I will give you a question copied from my mock exam. If the answer is a coding question, provide me very concise instructions to get the answer correct.
 # Use numbered bullet points. Say things like " use function '....' where you include the inputs (parameters) for me to copy and paste 
 #your last point might be copy and paste the output into the answer. 
+# In some questions, there may be ambiguity in which optionn is correct. In this case, you select the option which is best for the model
 #below i have copied the code for the integration section, that is all you will need. 
 #if the question does not require coding, simply provide me the answer and nothing else
